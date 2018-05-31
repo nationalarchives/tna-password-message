c49accddac9dd2bd77ebbf9d682c564bafebbbf4
Christopher Bishop <domingobishop@gmail.com>
2018-05-31 12:36:18 +0100

    html email

--- tna-password-message.php
+++ tna-password-message.php
@@ -9,14 +9,22 @@
  * License: GPL2
  */
 
+add_filter( 'wp_mail_content_type', 'wp_email_content_type_html' );
+
+function wp_email_content_type_html() {
+
+    return 'text/html';
+}
+
 add_filter('retrieve_password_message', 'custom_password_reset', 10, 4);
 
 function custom_password_reset( $message, $key, $user_login, $user_data )    {
 
-    $message =  __( 'Someone has requested a password reset for the following account:' ) . "\r\n\r\n";
-    $message .= sprintf(__('%s'), $user_data->user_email) . "\r\n\r\n";
-    $message .= __( 'If this was a mistake, just ignore this email and nothing will happen.' )  . "\r\n\r\n";
-    $message .= __( 'To reset your password, visit the following address:' )  . "\r\n\r\n";
+    $message =  __( '<h1>Password reset</h1>' );
+    $message .=  __( '<p>Someone has requested a password reset for the following account:</p>' );
+    $message .= '<p>' . sprintf(__('%s'), $user_data->user_email) . '</p>';
+    $message .= __( '<p>If this was a mistake, just ignore this email and nothing will happen.</p>' );
+    $message .= __( '<p>To reset your password, visit the following address:</p>' );
     $message .= network_site_url('wp-login.php?action=rp&key=' . rawurlencode($key) . '&login=' . rawurlencode($user_login), 'login');
 
     return $message;
